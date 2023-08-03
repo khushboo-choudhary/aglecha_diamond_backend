97a31adb68f64c730b657c60d4684f21aa2e5057
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-08-03 22:52:23 +0530

    Search functionality implemented

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const { debounce } = require("lodash");
+const debounce = require("lodash/debounce");
+
 const Todos = require("../models/ProductsModal");
-const e = require("express");
 
 const router = express.Router();
 
 router.post("", async (req, res) => {
   try {
-    console.log(req.body);
     const data = await Todos.create(req.body);
     return res.send(data);
   } catch (error) {
@@ -34,7 +33,6 @@ router.get("/id/:id", async (req, res) => {
     if (!data) {
       return res.status(404).send("Product not found.");
     }
-    // console.log("description", data);
     return res.send(data);
   } catch (error) {
     return res.send(error);
@@ -44,65 +42,37 @@ router.get("/id/:id", async (req, res) => {
 // Backend API endpoint for searching products
 
 // Debounce function to delay API calls when typing
+let previousSearchResults = [];
 const debouncedSearch = debounce(async (query, res) => {
   try {
-    console.log("hhuihiuhiu", query);
-
     if (query) {
       try {
         // If a search query is provided
         const regex = new RegExp(query, "i"); // Create a case-insensitive regex pattern from the search query
-        console.log("h=====================", regex);
         const data = await Todos.find().lean().exec();
         const filterCtegory = data.filter(
-          (e) =>
-            // console.log("djwkjw", e.description, "ndndnwjdnwjdw", searchQuery)
-            e.description === query ? e.description === query : e.tag === query,
-          e.tag === query
+          (e) => e.description.match(regex) || e.tag.match(regex)
         );
-        console.log("cheking resgwxsjkj", filterCtegory);
+        // Clear previous search results before updating with new ones
+        previousSearchResults = [];
+
+        // Update the previous search results with the new filtered results
+        previousSearchResults.push(...filterCtegory);
         return res.send(filterCtegory);
       } catch (error) {
         return res.status(500).json({ error: "Internal server error." });
       }
     } else {
-      return res.status(404).send("Not found product");
-    }
-    // } else {
-    //   // If no search query is provided, return all products
-    //   // data = await Todos.find().lean().exec();
-    //   return res.status(404).send("Not found product");
-    // }
-    // console.log("data", data);
-    // return res.send(data);
+      // If no search query is provided, return all products
+      const data = await Todos.find().lean().exec();
+      // Clear previous search results before updating with all products
+      previousSearchResults = [];
 
-    // if (query) {
-    //   //       // If a search query is provided
-    //   const regex = new RegExp(query, "i"); // Create a case-insensitive regex pattern from the search query
-    //   console.log("h=====================", regex);
-    //   const data = await Todos.find().lean().exec();
-    //   console.log("data", data);
-    //   const filterCtegory = data.filter(
-    //     (e) => console.log("djwkjw", e.description, "ndndnwjdnwjdw", regex),
-    //     e.description === regex
-    //   );
-    //   console.log("cheking resgwxsjkj", filterCtegory);
-    // } else {
-    //   // If no search query is provided, return all products
-    //   // data = await Todos.find().lean().exec();
-    //   return res.status(404).send("Not found product");
-    // }
-    // // console.log("data", data);
-    // return res.send(filterCtegory);
-    // const filterCtegory = datas.filter(
-    //   (e) =>
-    //     // console.log("djwkjw", e.description, "ndndnwjdnwjdw", searchQuery)
-    //     e.description === regex,
-    //   e.tag === regex
-    // );
-    // console.log("cheking resgwxsjkj", filterCtegory);
-    // return res.send(data);
-    // return res.send(data);
+      // Update the previous search results with all products
+      previousSearchResults.push(...data);
+      return res.send(data);
+      // return res.status(404).send("Not found product");
+    }
   } catch (error) {
     return res.status(500).send({ error: "Internal server error." });
   }
@@ -113,8 +83,7 @@ router.get("/search", async (req, res) => {
     const searchQuery = req.query.q;
 
     if (searchQuery) {
-      debouncedSearch(searchQuery, res);
-      // console.log("debounce", searchQuery, res); // Call the debounced function for real-time searching
+      debouncedSearch(searchQuery, res); // Call the debounced function for real-time searching
     } else {
       // If no search query is provided, return all products
       const data = await Todos.find().lean().exec();
@@ -124,34 +93,6 @@ router.get("/search", async (req, res) => {
     return res.status(500).json({ error: "Internal server error." });
   }
 });
-
-// router.get("/search", async (req, res) => {
-//   try {
-//     console.log("searching query", req.query);
-//     const searchQuery = req.query.q; // Get the search query from the request query parameters
-
-//     if (searchQuery) {
-//       // If a search query is provided
-//       const regex = new RegExp(searchQuery, "i"); // Create a case-insensitive regex pattern from the search query
-//       console.log("h=====================", regex);
-//       const data = await Todos.find().lean().exec();
-//       const filterCtegory = data.filter(
-//         (e) =>
-//           // console.log("djwkjw", e.description, "ndndnwjdnwjdw", searchQuery)
-//           e.description === searchQuery
-//       );
-//       console.log("cheking resgwxsjkj", filterCtegory);
-//     } else {
-//       // If no search query is provided, return all products
-//       // data = await Todos.find().lean().exec();
-//       return res.status(404).send("Not found product");
-//     }
-//     console.log("data", data);
-//     return res.send(data);
-//   } catch (error) {
-//     return res.status(500).send({ error: "Internal server error." });
-//   }
-// });
 
 // added filter of category discount rating sorting http://localhost:2345/product/earings?discount=60&rating=3.7
 router.get("/:id", async (req, res) => {
